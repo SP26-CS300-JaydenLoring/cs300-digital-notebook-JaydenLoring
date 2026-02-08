02bcba651483ef7ed71c1527a2b2341e0e9a5a13
Jayden Loring <jayskater05@gmail.com>
2026-02-08 13:32:49 -0600

    Add hello-node.js code snippet from week 1

---
+++ code-snippets/hello-node.js
@@ -0,0 +1,15 @@
+// hello-node.js
+// Simple Node.js example from Week 1
+
+console.log("Hello from Node.js!");
+console.log(`Running on Node version: ${process.version}`);
+
+// Arrow function example
+const greet = (name) => {
+    return `Welcome to CS 300, ${name}!`;
+};
+
+console.log(greet("Student"));
+
+nano code-snippets/hello-node.js
+
